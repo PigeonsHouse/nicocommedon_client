4d23c64cb8f360e7b13dd07eb33be028866c4bc4
PigeonsHouse <bullbull1230@gmail.com>
2021-11-20 16:14:13 +0900

    [add] create app!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const app = axios.create({
+    baseURL: 'https://mastodon.compositecomputer.club/api/v1/',
+    headers: {
+      'Authorization': 'Bearer hEWMfL3oK2WC87kbybWpK2CPlg_3vAQF7O8Xf8sbOkQ'
+    }
+  })
+
+  const [status, setStatus] = useState("");
+
+  const handleStatus = (e) => {
+    setStatus(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await app.post('https://mastodon.compositecomputer.club/api/v1/statuses', {
+      "status": status
+    })
+    console.log(res)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <h1 className="title">NicoCommeDon</h1>
+      <div className="content">
+        <p className="description">
+          C3部内で利用しているSNS，Mastodon！
+          Mastodonで流れるコメントや何気ない投稿をニコニコ動画のように流したい！
+          そんな思いでこのサイトを作成しました！
+          下のフォームに投稿するとMastodonに投稿できます！
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>※最長は500文字です．</p>
+        <form onSubmit={handleSubmit}>
+          <textarea value={status} onChange={handleStatus} className="textarea"></textarea>
+          <input className="submit_button" type="submit" value="トゥート！" />
+        </form>
+      </div>
+    </>
   );
 }
 
