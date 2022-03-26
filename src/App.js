1ddfb20dc28be142564d147a9c105bcb3dc9919d
PigeonsHouse <bullbull1230@gmail.com>
2022-03-26 18:58:05 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,40 +3,55 @@ import './App.css';
 import axios from "axios";
 
 function App() {
+  const [status, setStatus] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [errorDetail, setErrorDetail] = useState("");
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [token, setToken] = useState("");
+  const [timeStamp, setTimeStamp] = useState(null);
+
   const app = axios.create({
     baseURL: 'https://mastodon.compositecomputer.club/api/v1/',
     headers: {
-      'Authorization': 'Bearer epm3REnBEquk-P5SCnc5UvFCnEfzK500hjgMvdfNEdA'
+      'Authorization': `Bearer ${token}`
     }
   })
 
-  const [status, setStatus] = useState("");
-  const [isError, isSetError] = useState(false);
-  const [timeStamp, setTimeStamp] = useState(null);
-
   const handleStatus = (e) => {
     setStatus(e.target.value)
+  }
+
+  const handleToken = (e) => {
+    setToken(e.target.value)
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     let now = new Date();
     let nowTime = now.getTime();
-    console.log(timeStamp)
-    console.log(nowTime)
     if (timeStamp) {
       if (nowTime - timeStamp < 10000) {
-        isSetError(true)
+        setIsError(true)
+        setErrorDetail("投稿間隔が早すぎます．")
         return
       }
     }
-    isSetError(false)
+    setIsError(false)
     setTimeStamp(nowTime)
     const res = await app.post('https://mastodon.compositecomputer.club/api/v1/statuses', {
       "status": status
     })
     setStatus('')
     console.log(res)
+  }
+
+  const SignIn = () => {
+    setIsSignIn(true)
+  }
+
+  const SignOut = () => {
+    setToken("")
+    setIsSignIn(false)
   }
 
   return (
@@ -46,20 +61,39 @@ function App() {
         <p className="description">
           C3部内で利用しているSNS，Mastodon！
           Mastodonで流れるコメントや何気ない投稿をニコニコ動画のように流したい！
-          そんな思いでこのサイトを作成しました！
-          下のフォームに投稿するとMastodonに投稿できます！
+          そんな思いでこのアプリを作成しました！
+          ログインして下のフォームに投稿するとMastodonに投稿できます！
         </p>
-        <p>※最長は500文字です．</p>
         {
-          isError &&
-          <>
-            <p className="error">投稿頻度が早すぎます．</p>
-          </>
+          isSignIn ? (
+            <>
+              <p>※最長500文字です．</p>
+              {
+                isError &&
+                <>
+                  <p className="error">※{errorDetail}</p>
+                </>
+              }
+              <form onSubmit={handleSubmit}>
+                <textarea value={status} onChange={handleStatus} className="textarea"></textarea>
+                <label>コメントサイズ</label>
+                <input type="radio" name="size" value="mid" checked /><label for="mid">中</label>
+                <input type="radio" name="size" value="big" /><label for="big">大</label>
+                <input type="radio" name="size" value="sml" /><label for="sml">小</label>
+                <input className="submit_button" type="submit" value="トゥート！" />
+              </form>
+              <input className="submit_button" type="submit" value="サインアウト" onClick={SignOut} />
+            </>
+          ) : (
+            <>
+              <p>トークン</p>
+              <form onSubmit={SignIn}>
+                <input className="token_input" type="text" onChange={handleToken} />
+                <input class="submit_button" type="submit" value="サインイン" />
+              </form>
+            </>
+          )
         }
-        <form onSubmit={handleSubmit}>
-          <textarea value={status} onChange={handleStatus} className="textarea"></textarea>
-          <input className="submit_button" type="submit" value="トゥート！" />
-        </form>
       </div>
     </>
   );
